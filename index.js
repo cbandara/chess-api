75c94338e1f5bbc5949bd1b56744c9149d23c1fa
devin23 <devin@3droomspace.com>
2020-06-09 19:29:10 -0400

    added knight move logic

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const { Chess } = require('chess.js')
+
+
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-app.post("/hello", async (req, res) => {
+app.post("/knight", async (req, res) => {
   try {
+    const chess = new Chess()
+    chess.clear()
+
     const position = req.body.position
     console.log(position)
-    res.json(position);
+
+    chess.put({ type: chess.KNIGHT, color: chess.WHITE }, position)
+    console.log(chess.ascii())
+
+    const possibleMoves = chess.moves({ square: position }).map(m => m.substring(1).replace('#', ''));
+    console.log(possibleMoves)
+
+    res.json(possibleMoves);
   } catch (error) {
     console.error(err.message);
   }
@@ -19,4 +32,21 @@ app.post("/hello", async (req, res) => {
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
-}) 
+})
+
+// Import chess.jss
+
+// Get the Knight's current position fom the App request body
+
+// Create a list of possible moves
+// .moves([ options ])
+// Knight can move 2 squares horizontally and 1 square vertically
+// or 1 square horizontally and 2 squares vertically
+
+// Throw out any possible moves that are beyond the board
+// a-h and 1-8
+// Possibly use a grid setup of 64 squares???
+
+// Return list of positions in response body
+
+// Deploy API to heroku
